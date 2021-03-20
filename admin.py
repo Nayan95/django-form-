2a6ac9aaa8d982eb0b9cb802bb6caf6a6fa5840a
Nayan95 <44064755+Nayan95@users.noreply.github.com>
2021-03-20 23:15:24 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
 from .models import *
-@admin.register(Patient)
-@admin.register(Answer)
-@admin.register(QuesAns)
-@admin.register(Results)
-class UserAdmin(admin.ModelAdmin):
-    list_display=('Pid','name','age')
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('P_id', 'Ansid')
+    list_display=['Pid','name','age']
 
 class UserAdmin(admin.ModelAdmin):
-    list_display=('Qid','Question')
+    list_display = ['P_id', 'Ansid']
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('Q_id', 'Ansid', 'Answers')
+    list_display=['Qid','Question']
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['Q_id', 'Ansid', 'Answers']
 
 
+# Register your models here.
+admin.site.register(Patient)
+admin.site.register(Answer)
+admin.site.register(QuesAns)
+admin.site.register(Results)
 # Register your models here.
